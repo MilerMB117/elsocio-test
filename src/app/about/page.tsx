d20d1f441fd1d2982d93120c5f9704551e7cec0e
Miler Yesid Menjure Barrera <58488423+MilerMB117@users.noreply.github.com>
2024-06-21 11:56:34 -0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,19 +4,18 @@ import Image from 'next/image';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Author: React.FC = () => {
-    // Información estática del autor
     const name = "Miler Menjure Barrera";
     const bio = "System Engineer and Frontend Developer";
-    const avatar = "/avatar.webp"; 
+    const avatar = "/avatar.webp";
 
     return (
         <div className="min-h-screen bg-gray-100 pb-10">
             <Navbar />
-            <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-lg mt-8 md:mt-12"> 
+            <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-lg mt-8 md:mt-12">
                 <div className="flex flex-col md:flex-row items-center md:items-start">
                     <div className="md:w-1/3 mb-4 md:mb-0">
                         <div className="relative w-48 h-48 md:w-64 md:h-64 rounded-full overflow-hidden">
-                            <Image src={avatar} alt={name} layout="fill" objectFit="contain" className="rounded-full" />
+                            <Image src={avatar} alt={name} layout="fill" objectFit="cover" className="rounded-full" />
                         </div>
                     </div>
                     <div className="md:w-2/3 md:ml-8">
