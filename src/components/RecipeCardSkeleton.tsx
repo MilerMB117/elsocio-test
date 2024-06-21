6f0fe8b78e902840cdd025ee59c0372da32c414a
Miler Yesid Menjure Barrera <58488423+MilerMB117@users.noreply.github.com>
2024-06-21 13:01:05 -0500

    Update RecipeCardSkeleton.tsx

--- src/components/RecipeCardSkeleton.tsx
+++ src/components/RecipeCardSkeleton.tsx
@@ -3,16 +3,16 @@ import { FC } from 'react';
 interface RecipeCardSkeletonProps {
 
 }
-
+//Skeleton to animate load data and components
 const RecipeCardSkeleton: FC<RecipeCardSkeletonProps> = () => {
   return (
     <div className="bg-white shadow-lg overflow-hidden card">
-      <div className="animate-pulse w-full h-48 bg-gray-300"></div> {/* Placeholder for image */}
+      <div className="animate-pulse w-full h-48 bg-gray-300"></div> 
       <div className="p-4">
-        <div className="h-6 bg-gray-300 w-2/3 mb-2"></div> {/* Placeholder for title */}
-        <div className="h-4 bg-gray-300 w-1/2 mb-1"></div> {/* Placeholder for reviews */}
-        <div className="h-4 bg-gray-300 w-1/3"></div> {/* Placeholder for country and continent */}
-        <div className="h-4 bg-gray-300 w-1/4 mt-2"></div> {/* Placeholder for duration */}
+        <div className="h-6 bg-gray-300 w-2/3 mb-2"></div> 
+        <div className="h-4 bg-gray-300 w-1/2 mb-1"></div> 
+        <div className="h-4 bg-gray-300 w-1/3"></div> 
+        <div className="h-4 bg-gray-300 w-1/4 mt-2"></div> 
       </div>
     </div>
   );
